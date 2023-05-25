db1853584e36fa87b0caa54b6c6d87893dd3e3b4
Mohamad Akmal Ramadhanta <42136962+ramadhantaAkmal@users.noreply.github.com>
2023-05-25 19:00:06 +0700

    style: change main.dart title and readme.md file

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Bali Rent',
       theme: ThemeData(
         fontFamily: "NotoSans",
         primarySwatch: Colors.blue,
